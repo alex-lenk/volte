cd09cc6ea1fbaf66b02d38e6008fec7547609d66
s.yankovoy <a.yankovoy@smart-orange.biz>
2019-11-20 19:05:47 +0300

    Initial commit

---
+++ gulp/gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp'),
+    basePath = require('path'),
+    svgmin = require('gulp-svgmin'),
+    svgstore = require('gulp-svgstore');
+
+gulp.task('svgIcons', function () {
+    return gulp
+        .src('./icons/*.svg')
+        .pipe(svgmin(function (file) {
+            var prefix = basePath.basename(file.relative, basePath.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            }
+        }))
+        .pipe(svgstore())
+        .pipe(gulp.dest('./'));
+});
