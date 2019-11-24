600705f12546ff13f2432265f306641037f353ab
AlexLenk <alexlenk.ru@gmail.com>
2019-11-25 02:08:08 +0300

    Fixed minor bugs

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,16 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
         test: /\.(sass|scss)$/,
         include: path.resolve(__dirname, "src/scss"),
         use: [
